f27e1885ac497cdec64da5f2a12462fdd84a9a0a
darkcat-sudo <130399755+darkcat-sudo@users.noreply.github.com>
2023-05-09 08:28:29 +0600

    Add files via upload
    responsive change only

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
 
-function myLoad ()
+ function myLoad ()
   { let preload = document.getElementById('loding');
     let changeid = document.getElementById('ny');
       preload.style.display = 'none';
       changeid.setAttribute('id','chy');
 
    }
-
+ 
 function change() {
     let gt = document.querySelector(".listr");
-    gt.classList.toggle("listrtg")
+    let hyy = document.getElementById('dut');
+    gt.classList.toggle("listrtg");
+    
 }
 document.querySelector('.burger').addEventListener('click',change);
+
+
+function etu(){
+  let bt = document.querySelector("#bbd");
+  bt.classList.toggle("bdpart");
+}
